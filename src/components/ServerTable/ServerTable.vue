71000b4eb50c40e4bb38d8754dad443601b6eace
shmm20182018 <shanmiaomiao2007@126.com>
2018-06-08 08:49:49 +0800

    图表调整

--- src/components/ServerTable/ServerTable.vue
+++ src/components/ServerTable/ServerTable.vue
@@ -462,10 +462,12 @@ export default {
         height: 25px;
         line-height: 25px;
         padding-left: 4px;
-        font-size: 14px;
-        font-weight: 400;
+        font-size: 18px;
+        font-family: 'microsoftyahei';
+        font-weight:600;
         margin: 0;
-        text-align: center
+        text-align: center;
+        color: #666;
     }
     .exportbtn {
         position: absolute;
